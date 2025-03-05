8234298a47438a2f63937be20cc909a01424cc67
Avery-Moore <mooreakavery@outlook.com>
2025-03-05 13:35:02 -0800

    Added back + clear buttons, added circle

--- README.md
+++ README.md
@@ -6,8 +6,9 @@ used in the game "Tunic". Uses Tkinter for the UI.
 TODO:
 
 - Add the remaining rune translations
-- Add the little circle used for reversing the inner and outer runes
-- Add more text editing options (forward/back, clear, backspace)
+- ~~Add the little circle used for reversing the inner and outer runes~~
+- Add more text editing options (forward/back, ~~clear, backspace~~)
+- Add keyboard shortcuts
 - ~~Add a read-aloud text to speech button~~
 - Display previously inputted runes?
 - Add support for an IPA translation?
--- main.py
+++ main.py
@@ -15,6 +15,7 @@ def read_translation(text, tts):
     tts.say(text)
     tts.runAndWait()
 
+
 def create_outer_rune(rune):
     # ^
     rune.create_rune_line(250, 5, 200, 30)
@@ -25,6 +26,7 @@ def create_outer_rune(rune):
     # v
     rune.create_rune_line(200, 135, 250, 160)
     rune.create_rune_line(300, 135, 250, 160)
+
 
 def create_inner_rune(rune):
     # W
@@ -37,32 +39,58 @@ def create_inner_rune(rune):
     rune.create_rune_line(200, 135, 250, 110)
     rune.create_rune_line(250, 160, 250, 110)
     rune.create_rune_line(300, 135, 250, 110)
+    
+    
+def toggle_circle(canvas, circle):
+    if canvas.itemcget(circle, "outline") == "black":
+        canvas.itemconfigure(circle, outline="gray75")
+    else:
+        canvas.itemconfigure(circle, outline="black")
 
-def add_translation(text_widget):
+
+def is_circle_on(canvas, circle):
+    if canvas.itemcget(circle, "outline") == "black":
+        return True
+    else:
+        return False
+
+
+def add_translation(text_widget, canvas, circle):
     cur_text = text_widget.cget("text")
 
     inner_translation = ""
 
     with open("./data/inner_runes.json", "r") as f:
         data = json.load(f)
-        inner_translation = data.get(inner_rune.get_rune_string())
-
-    if inner_translation is not None:
-        cur_text += inner_translation
+        inner_translation = data.get(inner_rune.get_rune_string(), "")
 
     outer_translation = ""
 
     with open("./data/outer_runes.json", "r") as f:
         data = json.load(f)
-        outer_translation = data.get(outer_rune.get_rune_string())
+        outer_translation = data.get(outer_rune.get_rune_string(), "")
 
-    if outer_translation is not None:
-        cur_text += outer_translation
+    if is_circle_on(canvas, circle):
+        cur_text += outer_translation + inner_translation
+    else:
+        cur_text += inner_translation + outer_translation
 
     text_widget.configure(text = cur_text)
 
+
 def add_space(text_widget):
     text_widget.configure(text = text_widget.cget("text") + " ")
+
+
+def delete_word(text_widget):
+    cur_text = text_widget.cget("text")
+
+    split_text = cur_text.split(" ")
+
+    last_word = split_text[len(split_text) - 1]
+
+    text_widget.configure(text = cur_text[0:cur_text.index(last_word)])
+
 
 ttk.Label(text="Runes go Here").grid(column=0, row=0)
 
@@ -80,7 +108,13 @@ create_inner_rune(inner_rune)
 outer_rune = Rune(canvas)
 create_outer_rune(outer_rune)
 
-ttk.Button(root, text="Submit", command = lambda: add_translation(translation_text)).grid(column=0, row = 3)
+rune_circle = canvas.create_oval(225, 160, 275, 210, width=8, outline = "grey75")
+
+canvas.tag_bind(rune_circle, "<Button-1>", lambda x: toggle_circle(canvas, rune_circle))
+
+ttk.Button(root, text="Submit", command = lambda: add_translation(translation_text, canvas, rune_circle)).grid(column=0, row = 3)
 ttk.Button(root, text="Space", command = lambda: add_space(translation_text)).grid(column=1, row=3)
+ttk.Button(root, text="Delete", command = lambda: delete_word(translation_text)).grid(column=2, row=3)
+ttk.Button(root, text="Clear", command = lambda: translation_text.configure(text = "")).grid(column=3, row=3)
 
 root.mainloop()
